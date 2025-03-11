006133fada1301d47322060731904153f51a9ab6
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2025-03-11 13:50:55 +0300

    project created

---
+++ CryptoPocket/CryptoPocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoPocket
+//
+//  Created by Вадим Игнатенко on 11.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
